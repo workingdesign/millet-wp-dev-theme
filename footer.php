b98c472aee4c8bdcea051166864164d02ac39818
Giorgio Riccardi <griccardi@my.bcit.ca>
2015-06-16 22:15:24 -0700

    Added description, tags and screenshot

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'millet' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'millet' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'millet' ), 'millet', '<a href="http://underscores.me/" rel="designer">AlfredK</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'millet' ), 'millet', '<a href="http://giorgioriccardi.com/" rel="designer">Giorgio25b</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
